f99a2ddf0d47223bc30cf73c7f123dc1e4ff3ba6
jesari-git <158497071+jesari-git@users.noreply.github.com>
2024-10-21 17:53:08 +0200

    First version

--- test/tb.v
+++ test/tb.v
@@ -1,45 +1,188 @@
-`default_nettype none
-`timescale 1ns / 1ps
+//-------------------------------------------------------------------
+//-------------------------------------------------------------------
 
-/* This testbench just instantiates the module and makes some convenient wires
-   that can be driven / tested by the cocotb test.py.
-*/
-module tb ();
+`include "project.v"
 
-  // Dump the signals to a VCD file. You can view it with gtkwave.
-  initial begin
-    $dumpfile("tb.vcd");
-    $dumpvars(0, tb);
-    #1;
-  end
 
-  // Wire up the inputs and outputs:
-  reg clk;
-  reg rst_n;
-  reg ena;
-  reg [7:0] ui_in;
-  reg [7:0] uio_in;
-  wire [7:0] uo_out;
-  wire [7:0] uio_out;
-  wire [7:0] uio_oe;
+module tb();
 
-  // Replace tt_um_example with your module name:
-  tt_um_example user_project (
+parameter FCLK=24000000;
+parameter BAUD=1500000;
+localparam DIVI=(FCLK*10+(BAUD/2))/BAUD; 
 
-      // Include power ports for the Gate Level test:
-`ifdef GL_TEST
-      .VPWR(1'b1),
-      .VGND(1'b0),
-`endif
+//-- Registros con señales de entrada
+reg clk;
+reg reset;
+reg rxd;
+wire txd;
+wire pwmout;
 
-      .ui_in  (ui_in),    // Dedicated inputs
-      .uo_out (uo_out),   // Dedicated outputs
-      .uio_in (uio_in),   // IOs: Input path
-      .uio_out(uio_out),  // IOs: Output path
-      .uio_oe (uio_oe),   // IOs: Enable path (active high: 0=input, 1=output)
-      .ena    (ena),      // enable - goes high when design is selected
-      .clk    (clk),      // clock
-      .rst_n  (rst_n)     // not reset
-  );
+//-- Instanciamos
+wire [7:0]xd;
+wire xweb,xoeb,xbh,xlal,xlah;
+
+tt_um_gus16 #(.FCLK(FCLK), .BAUD(BAUD)) sys0 ( .clk(clk), .rst_n(~reset),
+	.ui_in(ui_in), .uo_out(uo_out), .uio_in(uio_in), .uio_out(uio_out), .uio_oe(uio_oe) 
+ );
+
+wire [7:0] ui_in;
+wire [7:0] uo_out;
+wire [7:0] uio_in;
+wire [7:0] uio_out;
+wire [7:0] uio_oe;
+
+assign {xlah,xlal,xbh}=uo_out[2:0];
+assign {txd,pwmout}=uo_out[4:3];
+assign {xweb,xoeb}=uo_out[7:6];
+
+// EXT RAM + latches
+reg [15:0]la=0;
+always @(posedge clk) if (xlal) la[7:0]<=uio_out;
+always @(posedge clk) if (xlah) la[15:8]<=uio_out;
+
+//latches transparentes tipo '373 (requiere --ignore-loops en nextpnr)
+//always @* if (xlal) la[7:0]<=xd;
+//always @* if (xlah) la[15:8]<=xd;
+
+wire [15:0]xa={la[14:0],xbh};
+
+reg [7:0]sram[0:65535];
+always @(negedge xweb) sram[xa]<=uio_out;
+assign uio_in=sram[xa];
+
+// Reloj periódico
+always #5 clk=~clk;
+
+//-- Proceso al inicio
+initial begin
+	//-- Fichero donde almacenar los resultados
+	$dumpfile("tb.vcd");
+	$dumpvars(0, tb);
+	reset = 1; clk=0; rxd=1;
+	#80 reset = 0;
+	
+	// 0xFF
+	#DIVI	rxd=0; // start
+	#DIVI	rxd=1;
+	#DIVI	rxd=1;
+	#DIVI	rxd=1;
+	#DIVI	rxd=1;
+	#DIVI	rxd=1;
+	#DIVI	rxd=1;
+	#DIVI	rxd=1;
+	#DIVI	rxd=1;
+	#DIVI	rxd=1; // Stop
+
+	// L (0x4C)
+	#DIVI	rxd=0; // start
+	#DIVI	rxd=0;
+	#DIVI	rxd=0;
+	#DIVI	rxd=1;
+	#DIVI	rxd=1;
+	#DIVI	rxd=0;
+	#DIVI	rxd=0;
+	#DIVI	rxd=1;
+	#DIVI	rxd=0;
+	#DIVI	rxd=1; // Stop
+	// 0x40
+	#DIVI	rxd=0; // start
+	#DIVI	rxd=0;
+	#DIVI	rxd=0;
+	#DIVI	rxd=0;
+	#DIVI	rxd=0;
+	#DIVI	rxd=0;
+	#DIVI	rxd=0;
+	#DIVI	rxd=1;
+	#DIVI	rxd=0;
+	#DIVI	rxd=1; // Stop
+	// 0x00
+	#DIVI	rxd=0; // start
+	#DIVI	rxd=0;
+	#DIVI	rxd=0;
+	#DIVI	rxd=0;
+	#DIVI	rxd=0;
+	#DIVI	rxd=0;
+	#DIVI	rxd=0;
+	#DIVI	rxd=0;
+	#DIVI	rxd=0;
+	#DIVI	rxd=1; // Stop
+
+	// 0x1
+	#DIVI	rxd=0; // start
+	#DIVI	rxd=1;
+	#DIVI	rxd=0;
+	#DIVI	rxd=0;
+	#DIVI	rxd=0;
+	#DIVI	rxd=0;
+	#DIVI	rxd=0;
+	#DIVI	rxd=0;
+	#DIVI	rxd=0;
+	#DIVI	rxd=1; // Stop
+	// 0x00
+	#DIVI	rxd=0; // start
+	#DIVI	rxd=0;
+	#DIVI	rxd=0;
+	#DIVI	rxd=0;
+	#DIVI	rxd=0;
+	#DIVI	rxd=0;
+	#DIVI	rxd=0;
+	#DIVI	rxd=0;
+	#DIVI	rxd=0;
+	#DIVI	rxd=1; // Stop
+
+	// 0x40
+	#DIVI	rxd=0; // start
+	#DIVI	rxd=0;
+	#DIVI	rxd=0;
+	#DIVI	rxd=0;
+	#DIVI	rxd=0;
+	#DIVI	rxd=0;
+	#DIVI	rxd=0;
+	#DIVI	rxd=1;
+	#DIVI	rxd=0;
+	#DIVI	rxd=1; // Stop
+	// 0x00
+	#DIVI	rxd=0; // start
+	#DIVI	rxd=0;
+	#DIVI	rxd=0;
+	#DIVI	rxd=0;
+	#DIVI	rxd=0;
+	#DIVI	rxd=0;
+	#DIVI	rxd=0;
+	#DIVI	rxd=0;
+	#DIVI	rxd=0;
+	#DIVI	rxd=1; // Stop
+	
+	// 0xFF
+	#DIVI	rxd=0; // start
+	#DIVI	rxd=1;
+	#DIVI	rxd=1;
+	#DIVI	rxd=1;
+	#DIVI	rxd=1;
+	#DIVI	rxd=1;
+	#DIVI	rxd=1;
+	#DIVI	rxd=1;
+	#DIVI	rxd=1;
+	#DIVI	rxd=1; // Stop
+	// 0xFF
+	#DIVI	rxd=0; // start
+	#DIVI	rxd=1;
+	#DIVI	rxd=1;
+	#DIVI	rxd=1;
+	#DIVI	rxd=1;
+	#DIVI	rxd=1;
+	#DIVI	rxd=1;
+	#DIVI	rxd=1;
+	#DIVI	rxd=1;
+	#DIVI	rxd=1; // Stop
+
+	//# 319 $display("FIN de la simulacion");
+	# 100000 $finish;
+	//# 1000 $finish;
+end
+
+
 
 endmodule
+
+
